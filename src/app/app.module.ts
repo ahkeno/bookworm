bf73952923d3934229fbf183d3582e69d46dc6e3
Chit Thiri Maung <thirimaung.chit@ext.asia.bnpparibas.com>
2018-07-16 13:30:27 +0800

    feat: adding basic route and dashboard page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule }	from './routes/app.route.module';
+
 import { AppComponent } from './app.component';
+import { DashboardPage } from './modules/dashboard/dashboard.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardPage
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
