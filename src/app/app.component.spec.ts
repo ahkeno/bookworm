ec05e77666040a177d3b7c00a5234f0d538e57aa
Chit <ahkeno@gmail.com>
2018-07-23 15:07:21 +0800

    fix: code review comment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,18 @@
-
 import { Location} from "@angular/common";
 import { TestBed, fakeAsync, tick,async} from '@angular/core/testing';
 import { RouterTestingModule} from "@angular/router/testing";
 import { Router} from "@angular/router";
 import { provideRoutes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-      imports: [ RouterTestingModule ]
+      imports: [ RouterTestingModule,CoreModule ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
--- src/app/components/searchBox/search-box.component.spec.ts
+++ src/app/components/searchBox/search-box.component.spec.ts
@@ -1,49 +1,7 @@
 
 import { TestBed, fakeAsync, tick,async} from '@angular/core/testing';
 import { Component } from '@angular/core';
-import { NgModule } from '@angular/core';
-import { CdkTableModule} from '@angular/cdk/table';
-import { CdkTreeModule} from '@angular/cdk/tree';
-import { BrowserModule } from '@angular/platform-browser';
-import {
-
-  MatAutocompleteModule,
-  MatBadgeModule,
-  MatBottomSheetModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatDividerModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatStepperModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatTreeModule,
-} from '@angular/material';
-
+import { CoreModule } from './../../core/core.module';
 import { SearchComponent } from './search-box.component';
 
 
@@ -54,41 +12,7 @@ describe('Search Box Compnent ', () => {
   beforeEach(async() =>{
   	 TestBed.configureTestingModule({
   	  imports: [
-  	  	MatAutocompleteModule,
-    		MatBadgeModule,
-    		MatBottomSheetModule,
-    		MatButtonModule,
-    		MatButtonToggleModule,
-    		MatCardModule,
-    		MatCheckboxModule,
-    		MatChipsModule,
-    		MatDatepickerModule,
-    		MatDialogModule,
-    		MatDividerModule,
-    		MatExpansionModule,
-    		MatGridListModule,
-    		MatIconModule,
-    		MatInputModule,
-    		MatListModule,
-    		MatMenuModule,
-    		MatNativeDateModule,
-    		MatPaginatorModule,
-    		MatProgressBarModule,
-    		MatProgressSpinnerModule,
-    		MatRadioModule,
-    		MatRippleModule,
-    		MatSelectModule,
-    		MatSidenavModule,
-    		MatSliderModule,
-    		MatSlideToggleModule,
-    		MatSnackBarModule,
-    		MatSortModule,
-    		MatStepperModule,
-    		MatTableModule,
-    		MatTabsModule,
-    		MatToolbarModule,
-    		MatTooltipModule,
-    		MatTreeModule
+  	  	CoreModule
   	  ],
       declarations: [
         SearchComponent,
